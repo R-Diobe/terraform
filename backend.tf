d6ab1de7fe7242b1b470a2b97d77addac604e3d6
R-Diobe <107433282+R-Diobe@users.noreply.github.com>
2022-11-26 16:21:59 -0600

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,10 +1,10 @@
- terraform {
-   backend "s3" {
-    bucket = "diobe-statefile-bucket"
-    key    = "state.tfstate"
-    region = "us-west-2"
-    profile = "default"
-dynamodb_table= "terraform-statefile-dynamodb"
-   }
- }
+#  terraform {
+#    backend "s3" {
+#     bucket = "diobe-statefile-bucket"
+#     key    = "state.tfstate"
+#     region = "us-west-2"
+#     profile = "default"
+# dynamodb_table= "terraform-statefile-dynamodb"
+#    }
+#  }
 
